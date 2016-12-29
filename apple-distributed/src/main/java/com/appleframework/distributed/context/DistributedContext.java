2cfb767ad69a5c1f95ae930195c93e3d2b16c99b
Cruise.Xu <xushaomin@foxmail.com>
2016-12-29 19:16:05 +0800

    0.1.7 新增Context上下文传输Operator信息

--- apple-distributed/src/main/java/com/appleframework/distributed/context/DistributedContext.java
+++ apple-distributed/src/main/java/com/appleframework/distributed/context/DistributedContext.java
@@ -3,16 +3,21 @@ package com.appleframework.distributed.context;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.appleframework.context.invoke.InvokeContext;
 import com.appleframework.model.Operator;
+import com.appleframework.model.OperatorType;
 
 public class DistributedContext implements InvokeContext {
 	
-	private static String KEY = "operator";
+	private static String KEY_ID   = "opid";
+	private static String KEY_TYPE = "optype";
 
 	public void setOperator(Operator operator) {
-		RpcContext.getContext().set(KEY, operator);
+		RpcContext.getContext().setAttachment(KEY_TYPE, String.valueOf(operator.getType().getIndex()));
+		RpcContext.getContext().setAttachment(KEY_ID, operator.getUserAsString());
 	}
 	
 	public Operator getOperator() {
-		return (Operator)RpcContext.getContext().get(KEY);
+		String id = RpcContext.getContext().getAttachment(KEY_ID);
+		String type = RpcContext.getContext().getAttachment(KEY_TYPE);
+		return Operator.creat(OperatorType.get(Integer.parseInt(type)), id);
 	}
 }
