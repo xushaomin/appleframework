46e752a1db4296e4f706697d8e7035e44396637e
xushaomin <xushaomin@foxmail.com>
2017-09-05 19:40:17 +0800

    update

--- apple-web/src/main/java/com/appleframework/web/springmvc/controller/AbstractController.java
+++ apple-web/src/main/java/com/appleframework/web/springmvc/controller/AbstractController.java
@@ -9,5 +9,9 @@ public abstract class AbstractController {
 	
 	protected static final ResponseEntity<WrapperResponseEntity> SUCCESS_MESSAGE = new ResponseEntity<WrapperResponseEntity>(
 			new WrapperResponseEntity(true), HttpStatus.OK);
+	
+	protected ResponseEntity<WrapperResponseEntity> createResponseEntity(Object data) {
+		return new ResponseEntity<WrapperResponseEntity>(new WrapperResponseEntity(data), HttpStatus.OK);
+	}
 
 }
