f83e864a8a72291ee06010062f21c4f6aae42f0b
xushaomin <xushaomin@vvsmart.com>
2018-08-11 21:09:29 +0800

    新增分布式ID生成器和分布式锁

--- apple-distributed/src/main/java/com/appleframework/distributed/zookeeper/election/ZKClientListener.java
+++ apple-distributed/src/main/java/com/appleframework/distributed/zookeeper/election/ZKClientListener.java
@@ -1,30 +1,24 @@
 package com.appleframework.distributed.zookeeper.election;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ZKClientListener implements LeaderLatchListener {
 
-	private static Log log = LogFactory.getLog(ZKClientListener.class);
-
-	private static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static Logger logger = LoggerFactory.getLogger(ZKClientListener.class);
 	
 	private ZkClientCache cache;
 	
 	@Override
 	public void isLeader() {
-		log.info(dateFormat.format(new Date()) + "当前服务已变为leader,将从事消费业务");
+		logger.info("当前服务已变为leader,将从事消费业务");
 		cache.setIsLeader(true);
 	}
 
 	@Override
 	public void notLeader() {
-		log.info(dateFormat.format(new Date()) + "当前服务已退出leader,不再从事消费业务");
+		logger.info("当前服务已退出leader,不再从事消费业务");
 		cache.setIsLeader(false);
 	}
 
